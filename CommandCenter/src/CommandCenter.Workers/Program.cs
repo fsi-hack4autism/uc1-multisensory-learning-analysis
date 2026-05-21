500aeb22adfb07b7e96fb2c1a5cc0ee433ac03d0
Gin <ginc11232@gmail.com>
2026-05-21 15:15:14 +0000

    Fixed worker healthcheck / expose port

--- CommandCenter/src/CommandCenter.Workers/Program.cs
+++ CommandCenter/src/CommandCenter.Workers/Program.cs
@@ -1,17 +1,17 @@
 using CommandCenter.Application;
 using CommandCenter.Infrastructure;
 using CommandCenter.Workers;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// Load secrets from GCP Secret Manager (no-op in Development unless SecretManager:Force=true)
 builder.AddGcpSecretManager();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddHostedService<SessionProcessingWorker>();
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+
+app.MapGet("/healthz", () => Results.Ok("healthy"));
+
+app.Run();
